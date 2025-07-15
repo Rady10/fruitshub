d9afe325d6379cac692b91a92d17e6e9b69e6acc
Rady10 <ahmedplays189@gmail.com>
2025-07-15 03:16:44 +0300

    firebase inti

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruitshub/core/helpers/on_generate_routes.dart';
 import 'package:fruitshub/core/services/shared_preferences_singleton.dart';
 import 'package:fruitshub/core/themes/pallete.dart';
 import 'package:fruitshub/features/splash/presentation/views/splash_view.dart';
+import 'package:fruitshub/firebase_options.dart';
 import 'package:fruitshub/generated/l10n.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs.init();
   runApp(const MyApp());
 }
