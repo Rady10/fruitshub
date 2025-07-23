4e3c7463337a1fd65bde99cd81d0139a819c1149
Rady10 <ahmedplays189@gmail.com>
2025-07-23 17:23:26 +0300

    added custom app bar and search field

---
+++ lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:fruitshub/core/themes/text_styles.dart';
+import 'package:fruitshub/core/utils/assets.dart';
+import 'package:svg_flutter/svg.dart';
+
+class CustomHomeAppBar extends StatelessWidget {
+  const CustomHomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        padding: const EdgeInsets.all(5),
+        child: Image.asset(Assets.imagesProfileImage)
+      ),
+      title: Text(
+        'صباح الخير !..', 
+        textAlign: TextAlign.right,
+        style: TextStyles.regular16.copyWith(
+          color: const Color(0xFF949D9E)
+        ),
+      ),
+      subtitle: const Text(
+        'احمد مصطفى', 
+        textAlign: TextAlign.right,
+        style: TextStyles.bold16
+      ),
+      trailing: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: const ShapeDecoration(
+          shape: OvalBorder(),
+          color: Color(0xFFEEF8ED)
+        ),
+        child: SvgPicture.asset(Assets.imagesNotification),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:fruitshub/core/utils/constants.dart';
+import 'package:fruitshub/core/widgets/search_text_field.dart';
+import 'package:fruitshub/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Column(
-        children: [
-          
+    return const Padding(
+      padding: const EdgeInsets.symmetric(horizontal: horizontalPadding),
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                SizedBox(height: 16,),
+                CustomHomeAppBar(),
+                SizedBox(height: 16,),
+                SearchTextField()
+              ],
+            ),
+          )
         ],
       ),
     );
